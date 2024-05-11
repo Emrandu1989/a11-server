4997b01be48898c97c55da4694c08b1bb339f713
Emrandu1989 <emrandu1989@gmail.com>
2024-05-11 12:09:06 +0600

    get specific data by using id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 7000;
@@ -47,6 +47,13 @@ async function run() {
 
      app.get('/foods', async(req, res)=>{
            const result = await foodCollection.find().toArray();
+           res.send(result)
+     })
+
+     app.get('/foods/:id', async(req, res)=>{
+          const id = req.params.id;
+           const query = {_id: new ObjectId(id)};
+           const result = await foodCollection.findOne(query);
            res.send(result)
      })
 
