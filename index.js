13ce5e3df63e25f77047c2dad2a447beecfc752e
Emrandu1989 <emrandu1989@gmail.com>
2024-05-12 16:05:21 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -7,14 +9,26 @@ const port = process.env.PORT || 7000;
 
 
 
-// const corsOptions = {
-//     origin: ['http://localhost:5173', 'http://localhost:5174'],
-//     credentials: true,
-//     optionSuccessStatus: 200,
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionSuccessStatus: 200,
 
-// }
-app.use(cors());
+}
+app.use(cors(
+  {
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      "https://b9-a11-b91e3.web.app",
+      "https://b9-a11-b91e3.web.app"
+     ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+));
 app.use(express.json());
+app.use(cookieParser())
 
 
 
@@ -31,6 +45,35 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares
+
+const logger = (req, res, next) =>{
+    console.log('log: info', req.method, req.url);
+       
+    next()
+}
+
+const verifyToken = (req, res, next) =>{
+     const token = req.cookies?.token;
+    //  console.log('token in the middleware', token);
+      
+    //  no token available
+    if(!token){
+        return  res.status(401).send({message: 'unauthorized access'})
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(401).send({message:'unauthorized access'})
+        }
+        req.user = decoded;
+
+        next()
+    })
+
+    //  next()
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,6 +81,31 @@ async function run() {
       
      const foodCollection = client.db("foodBD").collection("foods");
      const requestedFoodCollection = client.db("foodDb").collection('requestedFood')
+
+    //  auth related api
+     
+      app.post('/jwt',logger, async(req, res)=>{
+          const user = req.body;
+          console.log('user for token', user);
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1hr'})
+          res
+          .cookie('token', token,{
+              httpOnly:true,
+              secure:true,
+              sameSite:'none'
+
+          })
+          .send({success:true})
+      })
+
+      app.post('/logout', async(req, res)=>{
+          const user = req.body;
+          console.log('logging out', user)
+          res.clearCookie('token', {maxAge: 0}).send({success:true})
+      })
+
+
+    //  foods related api
 
      app.post('/foods', async(req, res)=>{
             const foodData = req.body;
@@ -88,7 +156,8 @@ async function run() {
 
     //  get all foods added by a specific user;
 
-    app.get('/food/:email', async(req, res)=>{
+    app.get('/food/:email', logger, verifyToken, async(req, res)=>{
+        //  console.log('cook cookies', req.cookies)
          const email = req.params.email;
          const query = {donatorEmail: email};
          const result = await foodCollection.find(query).toArray();
